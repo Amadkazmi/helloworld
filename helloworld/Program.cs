a89e4335035b33d85bfedafce31779f8ce735710
Amad <smart_kazmi@hotmail.com>
2022-09-28 19:12:05 +0200

    github

---
+++ helloworld/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+
+        string name, surname;
+        int age;
+        Console.WriteLine("Enter your name:");
+        name = Console.ReadLine();
+
+        Console.WriteLine("Enter your surname:");
+        surname = Console.ReadLine();
+        
+        Console.WriteLine("Enter your age:");
+        age = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("hello!");
+        Console.WriteLine("Your name is {0}, your surname is {1}, your age is {2}", name, surname, age);
+
+        
+
+
+    }
+}
